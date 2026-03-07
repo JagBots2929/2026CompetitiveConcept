4c6d59b9d1a3882d8a538b4e7bf62dedc10d48c8
Ian Rosenbery <ianrosenbery@proton.me>
2026-03-07 14:15:26 -0800

    shooter motors shoot same direction

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -24,7 +24,6 @@ import com.revrobotics.spark.config.SparkFlexConfig;
 
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.util.sendable.SendableBuilder;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
